2cd86d4c911ec4af7ff9b815b13de09963c88513
gsingh <gagankahlon036@gmail.com>
2025-02-14 13:29:26 -0600

    some minor changes in automatic_teller_machine.py file

--- automatic_teller_machine.py
+++ automatic_teller_machine.py
@@ -14,14 +14,46 @@ balance = random.randint(-1000, 10000)
 
 while True:
     os.system("cls" if os.name == "nt" else "clear")
-    print("****************************************")
-    print("          PIXELL RIVER FINANCIAL")
-    print("              AIM SIMULATOR")
+    print("*" * 40)
+    print("PIXELL RIVER FINANCIAL".center(40))
+    print("ATM Simulator".center(40))
     print()
-    print(f"  your current balance is: ${balance: ,.2f}")
+    print(f"Your current balance is: ${balance: ,.2f}". center(40))
     print()
-    print("                 Deposit: D")
-    print("                 Withdraw: W")
-    print("                 Quit: Q")
-    print("****************************************")
-    
+    print("Deposit: D".center(40))
+    print("Withdraw: W".center(40))
+    print("Quit: Q".center(40))
+    print("*" * 40)
+
+    selection = input("Enter your selection: ").strip().upper()
+
+    if selection not in ["D","W","Q"]:
+        print()
+        print("*" * 40)
+        print("INVALID SELECTION".center(40))
+        print("*" * 40)
+        sleep(3)
+        continue
+
+    elif selection == "Q":
+        break
+    amount = float(input("Enter your transaction amount: "))
+    if selection == "D":
+        balance += amount
+    elif selection == "W":
+        if amount > balance:
+            print()
+            print("*" * 40)
+            print("INSUFFICIENT FUNDS".center(40))
+            print("*" * 40)
+        else:
+            balance -= amount
+
+    print()
+    print("*" * 40)
+    print(f"Your current balance is: ${balance: ,.2f}".center(40))
+    print("*" * 40)
+    sleep(3)
+    os.system("cls" if os.name == "nt" else "clear")
+
+                
